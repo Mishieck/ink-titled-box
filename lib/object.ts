7721476d8857384e0ccddd763bfd26c9c2ab7a69
Mishieck <mishieckmwale@gmail.com>
2025-06-07 16:28:12 +0200

    feat(object): implement start, end, center, and space-between justifications

--- lib/object.ts
+++ lib/object.ts
@@ -11,6 +11,10 @@ import { borderCharacters } from "./data";
 
 export class TitledBoxApi implements TitledBoxData {
   static characters = borderCharacters;
+  /** The left and right padding of the title. */
+  static TITLE_PADDING = 2;
+  static TOP_CORNER_LENGTH = 2; // Top-left and Top-right characters
+  static TITLE_GAP = 1;
 
   style: BorderStyle;
   size: Size;
@@ -31,6 +35,26 @@ export class TitledBoxApi implements TitledBoxData {
       right: this.rightBorder,
       top: this.topBorder
     }
+  }
+
+  get visibleTitleCount(): number {
+    let count = 0;
+    let length = TitledBoxApi.TOP_CORNER_LENGTH;
+
+    while (count < this.titles.length) {
+      const title = this.titles[count]!;
+      length += title.length + TitledBoxApi.TITLE_PADDING;
+      if (count > 0) length += TitledBoxApi.TITLE_GAP;
+
+      if (length < this.size.width) count++;
+      else break;
+    }
+
+    return count;
+  }
+
+  get visibleTitles(): Array<string> {
+    return this.titles.slice(0, this.visibleTitleCount);
   }
 
   get characters(): BorderCharacters {
@@ -81,13 +105,95 @@ export class TitledBoxApi implements TitledBoxData {
     // NOTE: Should take title padding into account
 
     switch (this.titleJustify) {
-      case "flex-start": return [0];
-      case "flex-end": return [0];
-      case "space-between": return [0];
-      case "space-around": return [0];
-      case "space-evenly": return [0];
-      case "center": return [0];
+      case "flex-start": return this.startTitlePositions;
+      case "flex-end": return this.endTitlePositions;
+      case "space-between": return this.spaceBetweenTitlePositions;
+      case "space-around": return this.spaceAroundTitlePositions;
+      case "space-evenly": return this.spaceEvenlyTitlePositions;
+      case "center": return this.centerTitlePositions;
     }
+  }
+
+  get totalTitleLength(): number {
+    return this
+      .visibleTitles
+      .reduce(
+        (length, title) => length + title.length + TitledBoxApi.TITLE_PADDING,
+        0
+      );
+  }
+
+  get startTitlePositions(): Array<number> {
+    const positions: Array<number> = [];
+    let position = 0;
+
+    this.visibleTitles.forEach((title, i) => {
+      positions.push(position);
+      position += title.length + 3; // padding + gap = 3
+    });
+
+    return positions;
+  }
+
+  get endTitlePositions(): Array<number> {
+    const startPositions = this.startTitlePositions;
+    if (!startPositions.length) return startPositions;
+    const gapLength = startPositions.length - 1;
+    const padding = this.size.width - this.totalTitleLength - gapLength - TitledBoxApi.TOP_CORNER_LENGTH;
+    return startPositions.map(position => position + padding);
+  }
+
+  get spaceBetweenTitlePositions(): Array<number> {
+    const visibleTitles = this.visibleTitles;
+    const spaceCount = visibleTitles.length - 1;
+    const totalSpaceLength = this.size.width - this.totalTitleLength - 2;
+    const spaceLength = Math.floor(totalSpaceLength / spaceCount);
+    const remainderSpace = totalSpaceLength % spaceLength;
+    let positions: Array<number> = [];
+    let position = 0;
+
+    visibleTitles.forEach((title, i) => {
+      positions.push(position);
+      position += title.length + TitledBoxApi.TITLE_PADDING + spaceLength;
+    });
+
+    /* If there are spaces that have not been inserted, shift certain titles to
+     * the right by the number of spaces remaining. This ensures that the last
+     * title is always at the very end of the top border.
+     */
+    if (remainderSpace) {
+      const shiftPosition = Math.ceil(
+        (visibleTitles.length - remainderSpace) / 2
+      );
+
+      let remainingSpaces = remainderSpace;
+
+      while (remainingSpaces) {
+        for (let i = shiftPosition; i < visibleTitles.length; i++)
+          positions[i]!++;
+
+        remainingSpaces--;
+      }
+    }
+
+    return positions;
+  }
+
+  get spaceAroundTitlePositions(): Array<number> {
+    return [0];
+  }
+
+  get spaceEvenlyTitlePositions(): Array<number> {
+    return [0];
+  }
+
+  get centerTitlePositions(): Array<number> {
+    const startPositions = this.startTitlePositions;
+    if (!startPositions.length) return startPositions;
+    const gapLength = startPositions.length - 1;
+    const padding = this.size.width - this.totalTitleLength - gapLength - TitledBoxApi.TOP_CORNER_LENGTH;
+    const halfPadding = Math.floor(padding / 2);
+    return startPositions.map(position => position + halfPadding);
   }
 
   getVerticalBorder(character: string): string {
