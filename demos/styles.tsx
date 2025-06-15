3d88d8e854fa80931bcfac7ac4f168b5896fa860
Mishieck <mishieckmwale@gmail.com>
2025-06-15 16:34:57 +0200

    test(demos/styles): update styles for better presentation

--- demos/styles.tsx
+++ demos/styles.tsx
@@ -6,7 +6,7 @@ import { capitalize } from "./utils";
 const borderStyleNames = Object.keys(borderCharacters) as Array<BorderStyle>;
 
 export const Styles: React.FC = () => (
-  <Box flexDirection="column" gap={0}>
+  <Box width={60} padding={2} flexDirection="column" gap={1}>
     {
       borderStyleNames.map(name => (
         <TitledBox
